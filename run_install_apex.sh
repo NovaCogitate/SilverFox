bf3cec1abd3f2bfffbaed3b18c51e29774e93ca3
Pedro <joao.diasrodrigues@dkfz-heidelberg.de>
2023-12-16 23:57:16 +0100

    S

--- run_install_apex.sh
+++ run_install_apex.sh
@@ -7,4 +7,4 @@ cd $silver_path
 memory="24G"
 poetry_path="/dkfz/cluster/gpu/data/OE0094/j622s/poetry_home/bin/poetry"
 
-bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python ./install_apex.py 
+$poetry_path run python ./install_apex.py 
