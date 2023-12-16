11ebde9e921744571eea5bd0f12cb140ca55d8e3
Pedro <joao.diasrodrigues@dkfz-heidelberg.de>
2023-12-16 22:45:15 +0100

    s

---
+++ training_scripts/old_training_scripts/deploy_the_christmas_scripts.sh
@@ -0,0 +1,41 @@
+# remote server goto folder 
+silver_path="/home/j622s/alien_repo"
+cd $silver_path
+memory="24G"
+poetry_path="/dkfz/cluster/gpu/data/OE0094/j622s/poetry_home/bin/poetry"
+
+# 1 head
+path_to_script="/home/j622s/Desktop/SilverFox/training_scripts/old_training_scripts/96_channels/1_head/"
+
+script_name="train_alien_test_2D_256_8.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+script_name="train_alien_test_2D_256_16.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+script_name="train_alien_test_2D_256_16,8.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+# 2 head 
+path_to_script="/home/j622s/Desktop/SilverFox/training_scripts/old_training_scripts/96_channels/2_head/"
+
+script_name="train_alien_test_2D_256_8.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+script_name="train_alien_test_2D_256_16.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+script_name="train_alien_test_2D_256_16,8.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+# 3 head 
+path_to_script="/home/j622s/Desktop/SilverFox/training_scripts/old_training_scripts/96_channels/3_head/"
+
+script_name="train_alien_test_2D_256_8.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+script_name="train_alien_test_2D_256_16.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+
+script_name="train_alien_test_2D_256_16,8.py"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
