2aa1b7d2f06d2202eb4db2d79b46147aa3d4768a
Pedro <joao.diasrodrigues@dkfz-heidelberg.de>
2023-12-18 19:10:52 +0100

    added the 3heads

--- training_scripts/old_training_scripts/deploy_the_christmas_scripts_256.sh
+++ training_scripts/old_training_scripts/deploy_the_christmas_scripts_256.sh
@@ -9,14 +9,16 @@ path_to_script="/home/j622s/SilverFox/training_scripts/old_training_scripts/96_c
 script_name="train_alien_test_2D_256_16,8.py"
 
 # script_name="train_alien_test_2D_256_8.py"
-# bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+script_name="deploy_1.sh"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu bash $path_to_script/$script_name
 
 # script_name="train_alien_test_2D_256_16.py"
-# bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu $poetry_path run python $path_to_script/$script_name
+script_name="deploy_2.sh"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu bash $path_to_script/$script_name
 
-script_name="train_alien_test_2D_256_16,8.py"   
-# source /dkfz/cluster/gpu/data/OE0094/j622s/poetry_home/venv/bin/activate &&  
-bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu bash $path_to_script/deploy_3.sh
+# script_name="train_alien_test_2D_256_16,8.py"   
+script_name="deploy_3.sh"
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu bash $path_to_script/$script_name
 
 # 2 head 
 path_to_script="/home/j622s/SilverFox/training_scripts/old_training_scripts/96_channels/2_head"
