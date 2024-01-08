4ecded8cacbccab180d3bcf406309f38f601ebbc
Pedro <joao.diasrodrigues@dkfz-heidelberg.de>
2024-01-08 15:52:29 +0100

    x

--- training_scripts/post_christmas/2D/deploy_256_32.sh
+++ training_scripts/post_christmas/2D/deploy_256_32.sh
@@ -3,6 +3,6 @@ cd /home/j622s/SilverFox
 module load cudnn/8.5.0.96/cuda11
 module load gmp/6.1.0
 module load gcc/11.1.0
-path_to_script="/home/j622s/Desktop/SilverFox/training_scripts/post_christmas/2D"
+path_to_script="/home/j622s/SilverFox/training_scripts/post_christmas/2D"
 script_name="train_alien_test_2D_256_32,16.py"
 /home/j622s/.local/bin/poetry run python $path_to_script/$script_name
