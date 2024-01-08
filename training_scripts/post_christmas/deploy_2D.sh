e901238a286cc7f6cd7b91ba677992c516f8d3f4
Pedro <joao.diasrodrigues@dkfz-heidelberg.de>
2024-01-08 15:40:54 +0100

    s

--- training_scripts/post_christmas/deploy_2D.sh
+++ training_scripts/post_christmas/deploy_2D.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 cd /home/j622s/SilverFox
-module load cudnn/8.5.0.96/cuda11
-module load gmp/6.1.0
-module load gcc/11.1.0
 path_to_script="/home/j622s/Desktop/SilverFox/training_scripts/post_christmas/2D"
 
 script_name="deploy_256_16.sh"
-poetry run python $path_to_script/$script_name
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu bash $path_to_script/$script_name
 
 script_name="deploy_256_32.sh"
-poetry run python $path_to_script/$script_name
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu bash $path_to_script/$script_name
 
 script_name="deploy_512_16.sh"
-poetry run python $path_to_script/$script_name
+bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=$memory -R "rusage[mem=12GB]" -L /bin/bash -q gpu bash $path_to_script/$script_name
