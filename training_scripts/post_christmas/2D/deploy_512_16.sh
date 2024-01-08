3fd25fb889e5505280590f5e2d286085d56cab92
Pedro <joao.diasrodrigues@dkfz-heidelberg.de>
2024-01-08 15:50:07 +0100

    x

--- training_scripts/post_christmas/2D/deploy_512_16.sh
+++ training_scripts/post_christmas/2D/deploy_512_16.sh
@@ -5,4 +5,4 @@ module load gmp/6.1.0
 module load gcc/11.1.0
 path_to_script="/home/j622s/Desktop/SilverFox/training_scripts/post_christmas/2D"
 script_name="train_alien_test_2D_512_16,8.py"
-poetry run python $path_to_script/$script_name
+/home/j622s/.local/bin/poetry run python $path_to_script/$script_name
